dee6181030cd51e7a77450661f2880e11f99a95f
Michaeldremy <michaeldremy@gmail.com>
2020-10-30 21:21:42 -0700

    Added the ability to AddBook and AddAuthor

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ const RootQueryType = new GraphQLObjectType({
       type: BookType,
       description: "A Single Book",
       args: {
-        id: { type: GraphQLInt }
+        id: { type: GraphQLInt },
       },
-      resolve: (parent, args) => books.find(book => book.id === args.id),
+      resolve: (parent, args) => books.find((book) => book.id === args.id),
     },
     books: {
       type: new GraphQLList(BookType),
@@ -80,11 +80,58 @@ const RootQueryType = new GraphQLObjectType({
       description: "List of All Authors",
       resolve: () => authors,
     },
+    author: {
+      type: AuthorType,
+      description: "A Single Author",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
+    },
+  }),
+});
+
+// Mutation is similar to push, put, delete etc..
+const RootMutationType = new GraphQLObjectType({
+  name: "Mutation",
+  description: "Root Mutation",
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: "Add a book",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (parent, args) => {
+        const book = {
+          id: books.length + 1,
+          name: args.name,
+          authorId: args.authorId,
+        };
+        books.push(book);
+        return book;
+      },
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add a Author",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = { id: authors.length + 1, name: args.name, }
+        authors.push(author)
+        return author
+      },
+    },
   }),
 });
 
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  mutation: RootMutationType,
 });
 
 app.use(
