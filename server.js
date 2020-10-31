68ef9cedca7ffb059d6203f8a0ef7c95ef92f3e2
Michaeldremy <michaeldremy@gmail.com>
2020-10-30 20:36:11 -0700

    Added graphql and basic message

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-const express = require('express')
-const app = express()
-app.listen(5000, () => console.log('Server running'))
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Hello World",
+    fields: () => ({
+      message: { type: GraphQLString, resolve: () => "Hello World" },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+app.listen(5000, () => console.log("Server running"));
